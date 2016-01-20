59e43ced8dc7bdeb7714ae80e9e23d433e8214f8
sakaizawa <ksf.doingmorewithless@gmail.com>
2016-01-20 23:28:02 +0900

    テストケースを幾つか考える（デフォルトがFunSuiteだったのでその形式で）

---
+++ src/test/scala/com/github/dyamah/scalasom/VectorSuite.scala
@@ -0,0 +1,47 @@
+package com.github.dyamah.scalasom
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by sakaisawayuya on 2016/01/20.
+ */
+class VectorSuite extends FunSuite {
+
+  val vector1 = new VectorEx(3, 1)
+  val vector2 = new VectorEx(Array(1.0, 2.0, 3.0, 4.0))
+  val vector3 = new VectorEx(5, 1)
+
+
+  test("testDimension") {
+    assert(vector1.Dimension() == 3)
+    assert(vector2.Dimension() == 4)
+    assert(vector3.Dimension() == 5)
+  }
+
+  test("testGetElement") {
+    assert(vector1.getElement(0) == 1)
+    assert(vector1.getElement(1) == 1)
+    assert(vector1.getElement(2) == 1)
+
+    assert(vector2.getElement(0) == 1)
+    assert(vector2.getElement(1) == 2)
+    assert(vector2.getElement(2) == 3)
+    assert(vector2.getElement(3) == 4)
+
+    assert(vector3.getElement(0) == 1)
+    assert(vector3.getElement(1) == 1)
+    assert(vector3.getElement(2) == 1)
+    assert(vector3.getElement(3) == 1)
+    assert(vector3.getElement(4) == 1)
+
+  }
+
+  test("testAddElement") {
+
+  }
+
+  test("testSize") {
+    assert(vector1 == )
+  }
+
+}
