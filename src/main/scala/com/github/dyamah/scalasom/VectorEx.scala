974b47f23a897046c39b85318b6fbb597831d771
sakaizawa <ksf.doingmorewithless@gmail.com>
2016-01-26 12:15:44 +0900

    各メソッドの実装を追加

--- src/main/scala/com/github/dyamah/scalasom/VectorEx.scala
+++ src/main/scala/com/github/dyamah/scalasom/VectorEx.scala
@@ -3,43 +3,29 @@ package com.github.dyamah.scalasom
 /**
  * Created by sakaisawayuya on 2016/01/20.
  */
-class VectorEx (dim: Int, value: Double, _vector: Array[Double])  extends Vector {
+class VectorEx (vec : Seq[Double])  extends Vector {
 
-  val vector = new Array[Double](dim)
+  require(vec != None)
+  val vector = vec
+  def this(size: Int, value: Double) = this(Seq.fill(size)(value))
 
-  def this(dim: Int) = this(dim, null, null)
-  def this(dim: Int, value: Double) = this(dim, value, null)
-  def this(_vector: Array[Double]) = this(null, null, _vector)
+  /** このベクトルの要素数を返す
+    */
+  override def size: Int = vector.length
 
-  /**
-   * ベクトルを返す
-   * @return ベクトル
-   */
-  override def getVector(): Array[Double] = ???
+  /** i番目の要素の値を返す
+    *
+    * @param i 添字
+    */
+  override def apply(i: Int): Double = vector(i)
 
-  /**
-   * 次元を返す
-   * @return 次元
-   */
-  override def Dimension(): Int = ???
-
-  /**
-   * index 次元の値を返す
-   * @param index 次元
-   * @return index 次元の値
-   */
-  override def getElement(index: Int): Double = ???
-
-  /**
-   * index 次元の値に num を加える
-   * @param index インデックス
-   * @param num 加える数
-   */
-  override def addElement(index: Int, num: Double): Unit = ???
-
-  /**
-   * ベクトルの大きさを返す
-   * @return ベクトルの大きさ
-   */
-  override def Size(): Int = ???
+  /** このベクトルと内積値を返す
+    *
+    * @param that 内積を計算する対象のベクトル
+    */
+  override def *(that: Vector): Double = {
+    if (this.size != that.size) throw new Exception("Their length of the vector is different")
+    val inner = for (i <- 0 until this.size) yield this(i) * that(i)
+    inner.sum
+  }
 }
