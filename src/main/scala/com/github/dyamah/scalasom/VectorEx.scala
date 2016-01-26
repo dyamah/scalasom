49c52e4454c2e78b5e79fc57a1346a29a9d19317
sakaizawa <ksf.doingmorewithless@gmail.com>
2016-01-26 14:06:47 +0900

    実装追加（例外処理まとまられる？）

--- src/main/scala/com/github/dyamah/scalasom/VectorEx.scala
+++ src/main/scala/com/github/dyamah/scalasom/VectorEx.scala
@@ -5,7 +5,6 @@ package com.github.dyamah.scalasom
  */
 class VectorEx (vec : Seq[Double])  extends Vector {
 
-  require(vec != None)
   val vector = vec
   def this(size: Int, value: Double) = this(Seq.fill(size)(value))
 
@@ -27,5 +26,43 @@ class VectorEx (vec : Seq[Double])  extends Vector {
     if (this.size != that.size) throw new Exception("Their length of the vector is different")
     val inner = for (i <- 0 until this.size) yield this(i) * that(i)
     inner.sum
+    /* for の違う形？
+    var sum = 0.0
+    for (i <- 0 to this.size-1)
+      sum += this(i) * that(i)
+    sum
+     */
+  }
+
+  /** ベクトルを scalar 倍したベクトルを返す
+    *
+    * @param scalar 係数
+    * @return scalar 倍したベクトル
+    */
+  override def *(scalar: Double): Vector = {
+    val mulvec  = for (i <- 0 until this.size) yield scalar * this(i)
+    new VectorEx(mulvec)
+  }
+
+  /** このベクトルとの加算したベクトル返す
+    *
+    * @param that 加算を計算する対象のベクトル
+    * @return 加算した結果のベクトル
+    */
+  override def +(that: Vector): Vector = {
+    if (this.size != that.size) throw new Exception("Their length of the vector is different")
+    val addVec = for (i <- 0 until this.size) yield this(i) + that(i)
+    new VectorEx(addVec)
+  }
+
+  /** このベクトルとの減算したベクトル返す
+    *
+    * @param that 減算を計算する対象のベクトル
+    * @return 減算した結果のベクトル
+    */
+  override def -(that: Vector): Vector = {
+    if (this.size != that.size) throw new Exception("Their length of the vector is different")
+    val subVec = for (i <- 0 until this.size) yield this(i) - that(i)
+    new VectorEx(subVec)
   }
 }
