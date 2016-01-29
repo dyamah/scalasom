b26cd820f9d460923a9ef1795bfe3935ecb596c0
sakaizawa <ksf.doingmorewithless@gmail.com>
2016-01-29 15:54:41 +0900

    実行サンプルコード

--- src/main/scala/com/github/dyamah/scalasom/Sample.scala
+++ src/main/scala/com/github/dyamah/scalasom/Sample.scala
@@ -1,19 +1,42 @@
 package com.github.dyamah.scalasom
 
+import scala.util.Random
+
 /**
  * Created by sakaisawayuya on 2016/01/26.
  */
 object Sample {
   def main (args: Array[String]){
-    val v1 = new VectorEx(Seq.fill(5)(2))
-    println(v1.size)
-    println(v1(1))
+    /**
+    val v1 = new VectorImpl(Seq.fill(5)(2.0))
 
     val array = Array[Double](1, 2, 3, 4, 5)
-    val v2 = new VectorEx(array)
-    println(v2.size)
-    println(v2(3))
+    val v2 = new VectorImpl(array)
+
+    val list = List[Double](1, 2, 3, 4, 5)
+    val v3 = new VectorImpl(list)
+
+    val v4 = v1 * 2
+    val v5 = v1 + v2
+    val v6 = v1 - v2
 
     println(v1 * v2)
+    println(v2 * v3)
+    println(v4(0))
+    println(v1.distance(v1))
+    println(v1.distance(v2))
+      **/
+
+    val som = new SOM
+    val teachers = {
+      val init = new Array[Vector](1000)
+      for (i <- 0 until init.length) {
+        val teacher = for (j <- 0 until 3) yield Random.nextDouble()
+        init(i) = new VectorImpl(teacher)
+      }
+      init
+    }
+
+    println()
   }
 }
