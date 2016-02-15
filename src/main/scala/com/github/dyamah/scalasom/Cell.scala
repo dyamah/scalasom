6596ffba231aff731b45c76ebff2ea16a60c715a
sakaizawa <ksf.doingmorewithless@gmail.com>
2016-02-16 08:36:11 +0900

    メソッドを追加

--- src/main/scala/com/github/dyamah/scalasom/Cell.scala
+++ src/main/scala/com/github/dyamah/scalasom/Cell.scala
@@ -3,4 +3,6 @@ package com.github.dyamah.scalasom
 /**
  * Created by sakaisawayuya on 2016/02/02.
  */
-case class Cell(i: Int, j: Int, vector: Vector)
+case class Cell(i: Int, j: Int, vector: Vector) {
+  def distance(that: Cell) : Double = this.vector.distance(that.vector)
+}
